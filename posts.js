f183645cedc8f5f1fe03a0deb7173dc08ed0432e
rafi007akhtar <alimdrafi@gmail.com>
2019-09-08 22:25:54 +0530

    Modified posts with modularity

--- posts.js
+++ posts.js
@@ -6,17 +6,9 @@ let morgan = require("morgan");
 app.use(bp.json());
 app.use(morgan("dev"));
 
-let store = {
-	posts: [{
-		id: 1,
-		user: "rafi007akhtar",
-		text: "Traught, whelmed and feeling the aster",
-		comments: [
-			"today is the day",
-			"i hacked the mainframe"
-		]
-	}]
-};
+let index = require("./index");
+let store = index.store;
+let findPostInd = index.findPostInd;
 
 app.get("/posts", (req, res) => {
 	console.log(store);
@@ -29,8 +21,8 @@ app.post("/posts", (req, res) => {
 	return res.status(201).json({msg: "JSON received", store});
 });
 app.put("/posts/:id", (req, res) => {
-	let post = store.posts.filter(p => p.id == req.params.id)[0];
-	let ind = store.posts.indexOf(post);
+	// let post = store.posts.filter(p => p.id == req.params.id)[0];
+	let ind = findPostInd(req.params.id);
 	if (ind === -1)
 		return res.send("No such post present");
 	store.posts[ind] = req.body;  // complete replacement for put
@@ -38,8 +30,8 @@ app.put("/posts/:id", (req, res) => {
 	return res.status(201).json({msg: "JSON modified", store});
 });
 app.delete("/posts/:id", (req, res) => {
-	let post = store.posts.filter(p => p.id == req.params.id)[0];
-	let ind = store.posts.indexOf(post);
+	// let post = store.posts.filter(p => p.id == req.params.id)[0];
+	let ind = findPostInd(req.params.id);
 	if (ind === -1)
 		return res.send("No such post present\n");
 	store.posts.splice(ind, 1);
