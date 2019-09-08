16bc5ebe960e440a391367d5a14f36ee2c802091
rafi007akhtar <alimdrafi@gmail.com>
2019-09-08 23:27:55 +0530

    modularised comments route with server.js

--- comments.js
+++ comments.js
@@ -1,43 +1,33 @@
-let express = require("express");
-let app = express();
-let bp = require("body-parser");
-let morgan = require("morgan");
-
-app.use(bp.json());
-app.use(morgan("dev"));
-
-let index = require("./index");
+let index = require("./server");
 let store = index.store;
 let findPostInd = index.findPostInd;
 let findPost = index.findPost;
 
-app.get("/posts/:id/comments", (req, res) => {
-	let ind = findPostInd(req.params.id);
-	console.log(store.posts[ind]);
-	return res.status(200).json({"comments:": store.posts[ind].comments});
-});
-app.post("/posts/:id/comments", (req, res) => {
-	res.setHeader("Content-Type", "application/json");
-	let myPost = findPost(req.params.id);
-	myPost.comments.push(req.body.comment);  // make sure to post the data in {'comment': 'your comment'} format
-	console.log(store);
-	return res.send(myPost.comments);
-});
-app.put("/posts/:id/comments/:cid", (req, res) => {
-	res.setHeader("Content-Type", "application/json");
-	let myPost = findPost(req.params.id);
-	myPost.comments.splice(req.params.cid, 1, req.body.comment);  // assuming the comment id to be the index of the comment array
-	console.log(store);
-	return res.send(myPost.comments);	
-});
-app.delete("/posts/:id/comments/:cid", (req, res) => {
-	let myPost = findPost(req.params.id);
-	myPost.comments.splice(req.params.cid, 1);
-	console.log(store);
-	return res.send(myPost.comments);	
-});
+module.exports = {
+	getComments(req, res) {
+		let ind = findPostInd(req.params.id);
+		console.log(store.posts[ind]);
+		return res.status(200).json({"comments:": store.posts[ind].comments});
+	}, 
+	addComment(req, res) {
+		res.setHeader("Content-Type", "application/json");
+		let myPost = findPost(req.params.id);
+		myPost.comments.push(req.body.comment);  // make sure to post the data in {'comment': 'your comment'} format
+		console.log(store);
+		return res.send(myPost.comments);
+	},
+	updateComment(req, res) {
+		res.setHeader("Content-Type", "application/json");
+		let myPost = findPost(req.params.id);
+		myPost.comments.splice(req.params.cid, 1, req.body.comment);  // assuming the comment id to be the index of the comment array
+		console.log(store);
+		return res.send(myPost.comments);	
+	},
+	removeComment(req, res) {
+		let myPost = findPost(req.params.id);
+		myPost.comments.splice(req.params.cid, 1);
+		console.log(store);
+		return res.send(myPost.comments);
+	}  
+  }
 
-let [port, ip] = [process.env.PORT, process.env.IP];
-if (!port || !ip)
-	app.listen(4200, () => console.log("Server running at localhost:4200"));
-else app.listen(port, ip, () => console.log(`Server running at ${ip}:${port}`));
